c594918292992033c6ad429360cb20ddfee021e9
Jake Gordin <gcg0038@auburn.edu>
2025-04-13 15:15:50 -0500

    Initial Commit

---
+++ GordinNotes/GordinNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GordinNotesApp.swift
+//  GordinNotes
+//
+//  Created by Jake Gordin on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct GordinNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
